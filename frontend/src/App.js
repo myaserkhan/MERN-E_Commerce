b3c5639f154fcc7878cd5288da78b672812e439f
Yasir Khan <khanyaser007@gmail.com>
2022-11-05 16:59:51 +0500

    Project setup create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Endomaster</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
